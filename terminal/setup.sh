39411d2ee56bf5cfe7e1370d7c4ff9a84d15e5ee
Matthew Ephraim <me@mattephraim.com>
2019-05-23 07:28:35 -0700

    Add some terminal configuration stuff for enabling pretty colors

---
+++ terminal/setup.sh
@@ -0,0 +1,5 @@
+# See here for more information about seting up truecolors in the terminal
+# https://medium.com/@dubistkomisch/how-to-actually-get-italics-and-true-colour-to-work-in-iterm-tmux-vim-9ebe55ebc2be
+
+tic -x xterm-256color-italic.terminfo
+tic -x tmux-256color.terminfo
