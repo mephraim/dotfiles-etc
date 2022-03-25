a0f1d25e5b0852a60c31d798acab5b46bc4eb3d3
Matthew Ephraim <me@mattephraim.com>
2022-03-25 16:32:30 -0500

    Tweak completion and make it look a little nicer

--- vim/lua/plugins/language-server.lua
+++ vim/lua/plugins/language-server.lua
@@ -1,43 +1,72 @@
-lsp_installer = require("nvim-lsp-installer")
+local lsp_installer = require("nvim-lsp-installer")
 
-function config()
-  installServers()
-  setupServers()
+-- Custom icons for suggestion types
+local kind_icons = {
+  Class = "ﴯ",
+  Color = "",
+  Constant = "",
+  Constructor = "",
+  Enum = "",
+  EnumMember = "",
+  Event = "",
+  Field = "",
+  File = "",
+  Folder = "",
+  Function = "",
+  Interface = "",
+  Keyword = "",
+  Method = "",
+  Module = "",
+  Operator = "",
+  Property = "ﰠ",
+  Reference = "",
+  Snippet = "",
+  Struct = "",
+  Text = "",
+  TypeParameter = "",
+  Unit = "",
+  Value = "",
+  Variable = ""
+}
+
+-- These are the language servers we'd like to use
+local servers = {
+  "bashls",
+  "cssls",
+  "cucumber_language_server",
+  "dockerls",
+  "eslint",
+  "graphql",
+  "html",
+  "jsonls",
+  "lemminx",
+  "pyright",
+  "remark_ls",
+  "solargraph",
+  "sumneko_lua",
+  "tsserver",
+  "vimls",
+  "yamlls",
+}
+
+function ConfigLanguageServers()
+  InstallServers()
+  SetupServers()
+  SetupCompletion()
 end
 
-function installServers() 
-  -- These are the servers we want to install
-  local servers = {
-    "bashls",
-    "cssls",
-    "cucumber_language_server",
-    "dockerls",
-    "eslint",
-    "graphql",
-    "html",
-    "jsonls",
-    "lemminx",
-    "pyright",
-    "remark_ls",
-    "solargraph",
-    "sumneko_lua",
-    "tsserver",
-    "vimls",
-    "yamlls",
-  }
-
+-- Install of the language servers we want to use.
+function InstallServers()
   for _, name in pairs(servers) do
     local server_is_found, server = lsp_installer.get_server(name)
     if server_is_found and not server:is_installed() then
-      print("Installing " .. name)
       server:install()
     end
   end
 end
 
-function setupServers()
+function SetupServers()
   lsp_installer.on_server_ready(function(server)
-    print "I am ready "
     local opts = {}
 
     -- (optional) Customize the options passed to the server
@@ -52,12 +81,122 @@ function setupServers()
   end)
 end
 
+function SetupCompletion()
+  -- Add additional capabilities supported by nvim-cmp
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+  local lspconfig = require('lspconfig')
+
+  -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
+  for _, lsp in ipairs(servers) do
+    lspconfig[lsp].setup {
+      capabilities = capabilities,
+    }
+  end
+
+  -- luasnip setup
+  local luasnip = require('luasnip')
+
+  -- nvim-cmp setup
+  local cmp = require 'cmp'
+  cmp.setup {
+    snippet = {
+      expand = function(args)
+        luasnip.lsp_expand(args.body)
+      end,
+    },
+
+    -- Setup key mappings for the completion menu
+    mapping = {
+      ['<C-p>'] = cmp.mapping.select_prev_item(),
+      ['<C-n>'] = cmp.mapping.select_next_item(),
+      ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.close(),
+
+      ['<CR>'] = cmp.mapping.confirm {
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = true,
+      },
+
+      ['<Tab>'] = function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        else
+          fallback()
+        end
+      end,
+
+      ['<S-Tab>'] = function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end,
+    },
+
+    sources = {
+      { name = 'nvim_lsp' },
+      { name = 'luasnip' },
+      { name = 'buffer' },
+      { name = 'path' },
+      { name = 'cmdline' }
+    },
+
+    formatting = {
+      format = function(entry, vim_item)
+        -- Concatenate the kind icon and completion kind
+        vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind)
+
+        -- Source
+        vim_item.menu = ({
+          buffer = "[Buffer]",
+          nvim_lsp = "[LSP]",
+          luasnip = "[LuaSnip]",
+          nvim_lua = "[Lua]",
+          latex_symbols = "[LaTeX]",
+        })[entry.source.name]
+
+        return vim_item
+      end
+    },
+
+    experimental = {
+      ghost_text = true
+    }
+  }
+end
+
 return function(use)
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/cmp-path'
+
+  -- For autocompletions
+  use 'hrsh7th/nvim-cmp'
+
+  -- LSP source for nvim-cmp
+  use 'hrsh7th/cmp-nvim-lsp'
+
+ -- Snippets source for nvim-cmp
+  use 'saadparwaiz1/cmp_luasnip'
+
+  -- Snippets plugin
+  use 'L3MON4D3/LuaSnip'
+
   use {
+    -- Load standard lsconfig configurations
     'neovim/nvim-lspconfig',
     requires = {
       { 'williamboman/nvim-lsp-installer' }
     },
-    config = config
+    config = ConfigLanguageServers
   }
 end
