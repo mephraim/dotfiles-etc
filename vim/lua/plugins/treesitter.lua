e1d43cbbc2efee22e547b6537a57284c0f6552c4
Matthew Ephraim <me@mattephraim.com>
2022-08-17 20:26:20 -0500

    Setup the refactoring plugin

--- vim/lua/plugins/treesitter.lua
+++ vim/lua/plugins/treesitter.lua
@@ -15,4 +15,39 @@ return function(use)
     end,
     run = ":TSUpdate"
   }
+
+  use {
+    "ThePrimeagen/refactoring.nvim",
+    requires = {
+      {"nvim-lua/plenary.nvim"},
+      {"nvim-treesitter/nvim-treesitter"}
+    },
+    config = function()
+      require('refactoring').setup({})
+
+      -- Display a telescope window with the extract options
+      vim.api.nvim_set_keymap(
+	      "n",
+	      "<leader>rr",
+	      "<Esc><cmd>lua require('telescope').extensions.refactoring.refactors()<CR>",
+	      { noremap = true }
+      )
+
+      vim.api.nvim_set_keymap(
+	      "v",
+	      "<leader>rr",
+	      "<Esc><cmd>lua require('telescope').extensions.refactoring.refactors()<CR>",
+	      { noremap = true }
+      )
+
+      -- Remap in normal mode and passing { normal = true } will automatically find the variable under the cursor and print it
+      vim.api.nvim_set_keymap("n", "<leader>rv", ":lua require('refactoring').debug.print_var({ normal = true })<CR>", { noremap = true })
+
+      -- Remap in visual mode will print whatever is in the visual selection
+      vim.api.nvim_set_keymap("v", "<leader>rv", ":lua require('refactoring').debug.print_var({})<CR>", { noremap = true })
+
+      -- Cleanup function: this remap should be made in normal mode
+      vim.api.nvim_set_keymap("n", "<leader>rc", ":lua require('refactoring').debug.cleanup({})<CR>", { noremap = true })
+    end
+  }
 end
