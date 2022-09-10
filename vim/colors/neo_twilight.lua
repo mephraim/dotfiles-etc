abf790a74b429f5b5568b53f2f1e1fc6f234bf4e
Matthew Ephraim <me@mattephraim.com>
2022-09-09 23:04:21 -0500

    Create a collection of base colors

--- vim/colors/neo_twilight.lua
+++ vim/colors/neo_twilight.lua
@@ -16,20 +16,38 @@ local function hi(syntax, rules)
   vim.cmd(highlightCmd)
 end
 
+local colors = {
+  black = "#000000",
+  blue = "#7587A6",
+  brown = "#9B703F",
+  gray = "#828282",
+  green = "#8F9D6A",
+  light_blue = "#AFC4DB",
+  light_gray = "#666666",
+  light_green = "#DAEFA3",
+  light_orange = "#CDA869",
+  light_red = "#E04331",
+  light_yellow = "#F9EE98",
+  red = "#F11818",
+  salmon = "#CF6A4C",
+  yellow = "#EEEE00",
+  white = "#FFFFFF"
+}
+
 -- Syntax
-hi("Boolean", { guifg = "#E04331", gui = "underline" })
-hi("Comment", { guifg = "#828282" })
-hi("Constant", { guifg = "#CF6A4C" })
-hi("Define", { guifg = "#CDA869", gui = "none" })
-hi("Function", { guifg = "#9B703F" })
-hi("Identifier", { guifg = "#7587A6", gui = "none" })
-hi("Ignore", { guifg = "#666666" })
-hi("PreProc", { guifg = "#AFC4DB" })
-hi("Special", { guifg = "#DAEFA3" })
-hi("Statement", { guifg = "#CDA869" })
-hi("String", { guifg = "#8F9D6A" })
-hi("Todo", { guifg = "#EEEE00", guibg = "#000000", gui = "underline" })
-hi("Type", { guifg = "#F9EE98" })
+hi("Boolean", { guifg = colors.light_red, gui = "underline" })
+hi("Comment", { guifg = colors.gray })
+hi("Constant", { guifg = colors.salmon })
+hi("Define", { guifg = colors.light_orange, gui = "none" })
+hi("Function", { guifg = colors.brown })
+hi("Identifier", { guifg = colors.blue, gui = "none" })
+hi("Ignore", { guifg = colors.light_gray })
+hi("PreProc", { guifg = colors.light_blue })
+hi("Special", { guifg = colors.light_green })
+hi("Statement", { guifg = colors.light_orange })
+hi("String", { guifg = colors.green })
+hi("Todo", { guifg = colors.yellow, guibg = colors.black, gui = "underline" })
+hi("Type", { guifg = colors.light_yellow })
 
 -- Hide NonText characters by default.
 -- There's function that will make them visible in .vimrc.
@@ -37,7 +55,7 @@ hi("NonText", { guifg = "bg" })
 
  -- UI Elements
 hi("ColorColumn", { guibg = "#130707" })
-hi("Conceal", { guifg = '#FFFFFF', guibg = "bg" })
+hi("Conceal", { guifg = colors.white, guibg = "bg" })
 hi("Cursor",  { guifg = "bg", guibg = "#F5773c",  gui = "NONE" })
 hi("CursorColumn", { guibg = "#2D2D2D" })
 hi("CursorLine", { guibg = "#131313", gui = "bold" })
@@ -45,33 +63,37 @@ hi("Directory", { guifg = "#CFCFCF" })
 hi("FoldColumn", { guifg = "#444444", guibg = "#080808" })
 hi("Folded",  { guibg = "bg", guifg = "#434343", gui = "italic" })
 hi("LineNr", { guibg = "bg", guifg = "#434343" })
-hi("MatchParen",  { guifg= "#E04331", guibg = "bg" })
+hi("MatchParen",  { guifg= colors.red, guibg = "bg" })
 hi("Normal", { guibg = "#1D1D1D", gui = "NONE" })
 hi("Pmenu",  { guifg = "#F6F3E8", guibg = "#242829" })
-hi("PmenuSel", { guifg = "#000000", guibg = "#cae682" })
+hi("PmenuSel", { guifg = "#000000", guibg = colors.light_green })
 hi("SignColumn", { guibg="#1D1D1D" })
-hi("TabLineSel", { guifg = "#D4EB9C" })
+hi("TabLineSel", { guifg = colors.light_green })
 hi("VertSplit", { guifg = "#181B22", guibg="bg", gui = "NONE" })
 hi("Visual",  { guifg = "NONE", guibg = "#1F4457", gui = "none" })
-hi("WildMenu", { guifg = "#E04331", guibg = "bg" })
+hi("WildMenu", { guifg = colors.light_red, guibg = "bg" })
+
+-- https://github.com/nvim-treesitter/nvim-treesitter/blob/master/lua/nvim-treesitter/highlight.lua
+hi("TSConstant", { guifg = colors.light_red, gui = "bold" })
+hi("TSKeywordReturn", { gui = "italic" })
 
 -- Warnings and Errors
-hi("Error", { guifg="#F11818", guibg = "bg" })
+hi("Error", { guifg = colors.red, guibg = "bg" })
 hi("Warning", { guifg = "#E7F6DA", guibg = "#3A0505" })
-hi("WarningMsg", { guifg = "#F11818", guibg = "bg" })
+hi("WarningMsg", { guifg = colors.light_yellow, guibg = "bg" })
 
 -- Status Line
-hi("StatusLine", { guifg = "#666666", guibg = "bg", gui = "NONE" })
+hi("StatusLine", { guifg = colors.light_gray, guibg = "bg", gui = "NONE" })
 hi("StatusLineFile",  { guibg = "bg" })
 hi("StatusLineFileType", { guifg = "#8AC6F2", guibg = "bg" })
 hi("StatusLineGit", { guifg = "#D4EB9C", guibg = "bg" })
 hi("StatusLineNC", { guifg = "bg", guibg = "bg" })
 
 -- Diff highlighting
-hi("DiffAdd", { guibg = "#D1FFB6", guifg = "#080C06" })
-hi("DiffChange", { guibg = "#0F1308", guifg = "NONE" })
+hi("DiffAdd", { guibg = colors.light_green, guifg = "#080C06" })
+hi("DiffChange", { guibg = colors.light_orange, guifg = colors.black })
 hi("DiffDelete", { guibg = "#290700", guifg = "#290700" })
-hi("DiffText",  { guibg = "#D1FFB6", guifg = "#080C06" })
+hi("DiffText",  { guibg = colors.green, guifg = "#080C06" })
 
 -- Terminal colors
 hi("Terminal", { guibg = "#111720", guifg = "fg" })
@@ -96,5 +118,5 @@ hi("GitGutterChange", { guifg = "#f5773C", guibg = "NONE" })
 hi("GitGutterChangeDelete",  { guifg = "#f5773C", guibg = "NONE" })
 hi("GitGutterChangeDeleteLine", { guibg = "#191919" })
 hi("GitGutterChangeLine", { guibg = "#191919" })
-hi("GitGutterDelete", { guifg = "#F11818" })
+hi("GitGutterDelete", { guifg = colors.red })
 hi("GitGutterDeleteLine", { guibg = "#191919" })
