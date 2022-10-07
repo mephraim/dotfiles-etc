8df735a5b7475abf84a30ac0eab5bb3804f5c47b
Matthew Ephraim <me@mattephraim.com>
2022-10-07 12:15:40 -0500

    Start using the UFO plugin for folds

--- vim/lua/plugins/ui.lua
+++ vim/lua/plugins/ui.lua
@@ -58,4 +58,32 @@ return function(use)
       vim.notify = notify
     end
   }
+
+  use {
+    'kevinhwang91/nvim-ufo',
+    requires = 'kevinhwang91/promise-async',
+    config = function()
+      require('ufo').setup({
+        provider_selector = function()
+          return {'treesitter', 'indent'}
+        end
+      })
+
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+
+      vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+      vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+      vim.keymap.set('n', 'zr', require('ufo').openFoldsExceptKinds)
+      vim.keymap.set('n', 'zm', require('ufo').closeFoldsWith)
+
+      vim.keymap.set('n', 'K', function()
+        local winid = require('ufo').peekFoldedLinesUnderCursor()
+        if not winid then
+          vim.lsp.buf.hover()
+        end
+      end)
+    end
+  }
 end
