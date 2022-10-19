3da1e3996d78d45829c7ba9bb31ae759c3e2da86
Matthew Ephraim <me@mattephraim.com>
2022-10-18 19:45:06 -0500

    Replace the deprecated `update_capabilities` call

--- vim/lua/plugins/language-server.lua
+++ vim/lua/plugins/language-server.lua
@@ -46,7 +46,8 @@ function Config()
   SetupServers()
   SetupMappings()
   SetupUI()
-  SetupLSPProgressNotifications()
+  -- Disable this because it seems to have some conflict with noice
+  -- SetupLSPProgressNotifications()
   SetupCompletion()
 end
 
@@ -73,7 +74,7 @@ function SetupServers()
   })
 
   local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+  capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
   require("mason-lspconfig").setup_handlers({
     function (server_name)
