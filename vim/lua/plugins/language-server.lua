efdf6b57830eb540bba7891cb99c704f567d067d
Matthew Ephraim <me@mattephraim.com>
2023-07-02 16:46:30 -0500

    Cleanup the language server configuration a bit
    Also display a popup with the full diagnostic message when the cursor is
    over the diagnostic error.

--- vim/lua/plugins/language-server.lua
+++ vim/lua/plugins/language-server.lua
@@ -80,9 +80,8 @@ function Config()
   SetupServers()
   SetupMappings()
   SetupUI()
-  -- Disable this because it seems to have some conflict with noice
-  -- SetupLSPProgressNotifications()
   SetupCompletion()
+  SetupDiagnosticFloat()
 end
 
 function SetupMappings()
@@ -334,104 +333,45 @@ function SetupUI()
       max_width = 120
     }
   )
-
-  -- local severity = {
-  --   "error",
-  --   "warn",
-  --   "info",
-  --   "info", -- map both hint and info to info?
-  -- }
-
-  -- vim.lsp.handlers["window/showMessage"] = function(_, method, params, _)
-  --   vim.notify(method.message, severity[params.type], {
-  --     title = 'LSP message',
-  --     timeout = 1000
-  --   })
-  -- end
 end
 
-function SetupLSPProgressNotifications()
-  local client_notifs = {}
-
-  local function get_notif_data(client_id, token)
-    if not client_notifs[client_id] then
-      client_notifs[client_id] = {}
+function SetupDiagnosticFloat()
+  function OpenDiagnosticFloatWindow()
+    -- Don't open the float if there's an existing float window
+    for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+      if vim.api.nvim_win_get_config(winid).zindex then
+        return
+      end
     end
 
-    if not client_notifs[client_id][token] then
-      client_notifs[client_id][token] = {}
-    end
-
-    return client_notifs[client_id][token]
+    vim.diagnostic.open_float(0, {
+      focusable = false,
+      header = " Diagnostics",
+      pad_top = 1,
+      pad_bottom = 1,
+      scope = "cursor",
+      wrap = true,
+      close_events = {
+        "CursorMoved",
+        "CursorMovedI",
+        "BufHidden",
+        "InsertCharPre",
+        "WinLeave",
+      },
+    })
   end
 
-  local spinner_frames = { "⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷" }
-  local function update_spinner(client_id, token)
-    local notif_data = get_notif_data(client_id, token)
+  vim.api.nvim_create_augroup("LSPDiagnosticsFloat", {
+    clear = true
+  })
 
-    if notif_data.spinner then
-      local new_spinner = (notif_data.spinner + 1) % #spinner_frames
-      notif_data.spinner = new_spinner
-
-      notif_data.notification = vim.notify(nil, nil, {
-        hide_from_history = true,
-        icon = spinner_frames[new_spinner],
-        replace = notif_data.notification,
-      })
-
-      vim.defer_fn(function()
-        update_spinner(client_id, token)
-      end, 100)
-    end
-  end
-
-  local function format_title(title, client_name)
-    return client_name .. (#title > 0 and ": " .. title or "")
-  end
-
-  local function format_message(message, percentage)
-    return (percentage and percentage .. "%\t" or "") .. (message or "")
-  end
-
-  vim.lsp.handlers["$/progress"] = function(_, result, ctx)
-    local client_id = ctx.client_id
-    local val = result.value
-
-    if not val.kind then
-      return
-    end
-
-    local notif_data = get_notif_data(client_id, result.token)
-
-    if val.kind == "begin" then
-      local message = format_message(val.message, val.percentage)
-
-      notif_data.notification = vim.notify(message, "info", {
-        title = format_title(val.title, vim.lsp.get_client_by_id(client_id).name),
-        icon = spinner_frames[1],
-        timeout = false,
-        hide_from_history = false,
-      })
-
-      notif_data.spinner = 1
-      update_spinner(client_id, result.token)
-    elseif val.kind == "report" and notif_data then
-      notif_data.notification = vim.notify(format_message(val.message, val.percentage), "info", {
-        replace = notif_data.notification,
-        hide_from_history = false,
-      })
-    elseif val.kind == "end" and notif_data then
-      notif_data.notification =
-      vim.notify(val.message and format_message(val.message) or "Complete", "info", {
-        icon = "",
-        replace = notif_data.notification,
-        timeout = 2000,
-      })
-
-      notif_data.spinner = nil
-    end
-  end
+  vim.api.nvim_create_autocmd({ "CursorHold" }, {
+    pattern = "*",
+    callback = OpenDiagnosticFloatWindow,
+    group = "LSPDiagnosticsFloat",
+  })
 end
+
 
 return function(use)
   local enable_virtual_lines = false
