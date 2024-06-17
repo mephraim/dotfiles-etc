b6ccced0369afefde7e9215e0e19014dde7742cd
Matthew Ephraim <me@mattephraim.com>
2024-06-17 11:16:12 -0500

    Get rid of some treesitter plugins I don't use

--- vim/lua/plugins/treesitter.lua
+++ vim/lua/plugins/treesitter.lua
@@ -59,84 +59,9 @@ return function(use)
           enable = true,
           use_virtual_text = true,
           lint_events = {"BufWrite", "CursorHold"},
-        },
-        textobjects = {
-          select = {
-            enable = true,
-            keymaps = {
-              ["af"] = {
-                query = "@function.outer",
-                desc = "Select outer part of a function"
-              },
-              ["if"] = {
-                query = "@function.inner",
-                desc = "Select inner part of a function"
-              },
-              ["ac"] = {
-                query = "@class.outer",
-                desc = "Select outer part of a class region"
-              },
-              ["ic"] = {
-                query = "@class.inner",
-                desc = "Select inner part of a class region"
-              },
-            }
-          },
-          swap = {
-            enable = true,
-            swap_next = {
-              ["<leader>sp"] = "@parameter.inner",
-            },
-            swap_previous = {
-              ["<leader>SP"] = "@parameter.inner",
-            },
-          },
-        },
+        }
       }
     end
-  }
-
-  use {
-    "ThePrimeagen/refactoring.nvim",
-    requires = {
-      {"nvim-lua/plenary.nvim"},
-      {"nvim-treesitter/nvim-treesitter"}
-    },
-    config = function()
-      require('refactoring').setup({})
-
-      -- Display a telescope window with the extract options
-      vim.api.nvim_set_keymap(
-        "n",
-        "<leader>rr",
-        "<Esc><cmd>lua require('telescope').extensions.refactoring.refactors()<CR>",
-        { noremap = true }
-      )
-
-      vim.api.nvim_set_keymap(
-        "v",
-        "<leader>rr",
-        "<Esc><cmd>lua require('telescope').extensions.refactoring.refactors()<CR>",
-        { noremap = true }
-      )
-
-      -- Remap in normal mode and passing { normal = true } will automatically find the variable under the cursor and print it
-      vim.api.nvim_set_keymap("n", "<leader>rv", ":lua require('refactoring').debug.print_var({ normal = true })<CR>", { noremap = true })
-
-      -- Remap in visual mode will print whatever is in the visual selection
-      vim.api.nvim_set_keymap("v", "<leader>rv", ":lua require('refactoring').debug.print_var({})<CR>", { noremap = true })
-
-      -- Cleanup function: this remap should be made in normal mode
-      vim.api.nvim_set_keymap("n", "<leader>rc", ":lua require('refactoring').debug.cleanup({})<CR>", { noremap = true })
-    end
-  }
-
-  use {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    after = 'nvim-treesitter',
-    requires = {
-      "nvim-treesitter"
-    }
   }
 
   use {
