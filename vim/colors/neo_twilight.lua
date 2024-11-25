12fa585a807f0ba0a2581f141e8221237caff29d
Matthew Ephraim <me@mattephraim.com>
2024-11-25 10:47:28 -0600

    Update my colorscheme to use treesitter matches

--- vim/colors/neo_twilight.lua
+++ vim/colors/neo_twilight.lua
@@ -26,6 +26,7 @@ local colors = {
   light_red = "#E04331",
   light_yellow = "#F9EE98",
   middle_gray = "#2A2A2A",
+  orange = "#F5773C",
   red = "#F11818",
   salmon = "#CF6A4C",
   yellow = "#EEEE00",
@@ -33,19 +34,75 @@ local colors = {
 }
 
 -- Syntax
+-- https://github.com/nvim-treesitter/nvim-treesitter/blob/master/lua/nvim-treesitter/highlight.lua
 hi("Boolean", { fg = colors.light_red, underline = true })
+hi("TSBoolean", { fg = colors.light_red, underline = true })
+
 hi("Comment", { fg = colors.gray })
+hi("TSComment", { fg = colors.gray })
+hi("@comment", { fg = colors.light_gray, italic = true })
+
 hi("Constant", { fg = colors.salmon })
+hi("TSConstant", { fg = colors.light_red })
+
 hi("Define", { fg = colors.light_orange })
+hi("TSDefine", { fg = colors.light_orange })
+
 hi("Function", { fg = colors.brown })
+hi("TSFunction", { fg = colors.brown })
+
 hi("Identifier", { fg = colors.blue })
+hi("TSField", { fg = colors.blue })
+hi("TSSymbol", { fg = colors.blue })
+
+hi("TSParameter", { fg = colors.light_blue })
+hi("@variable.parameter", { fg = colors.light_blue })
+hi("@variable", { fg = colors.light_yellow })
+
+hi("TSProperty", { fg = colors.blue })
+hi("@property", { fg = colors.blue })
+
 hi("Ignore", { fg = colors.light_gray })
+
 hi("PreProc", { fg = colors.light_blue })
+hi("TSPreProc", { fg = colors.light_blue })
+hi("TSAnnotation", { fg = colors.light_blue })
+hi("TSAttribute", { fg = colors.light_blue })
+
 hi("Special", { fg = colors.light_green })
+hi("TSCharacterSpecial", { fg = colors.light_green })
+hi("TSConstBuiltin", { fg = colors.light_green })
+hi("TSConstructor", { fg = colors.light_green })
+hi("TSFuncBuiltin", { fg = colors.light_green })
+hi("TSMath", { fg = colors.light_green })
+hi("TSPunctSpecial", { fg = colors.light_green })
+hi("TSStringSpecial", { fg = colors.light_green })
+hi("TSVariableBuiltin", { fg = colors.light_green })
+
 hi("Statement", { fg = colors.light_orange })
-hi("String", { fg = colors.green })
+hi("TSString", { fg = colors.green })
+hi("TSStringRegex", { fg = colors.green })
+hi("TSLiteral", { fg = colors.green })
+hi("@string", { fg = colors.green })
+
 hi("Todo", { fg = colors.yellow, bg = colors.black, underline = true })
+hi("TSTodo", { fg = colors.yellow, bg = colors.black, underline = true })
+hi("TSWarning", { fg = colors.yellow, bg = colors.black, underline = true })
+
 hi("Type", { fg = colors.light_yellow })
+hi("TSEnvironmentName", { fg = colors.light_yellow })
+hi("TSType", { fg = colors.light_yellow })
+hi("TSTypeBuiltin", { fg = colors.light_yellow })
+hi("TSTypeQualifier", { fg = colors.light_yellow })
+
+hi("TSKeywordReturn", { fg = colors.salmon, italic = true })
+
+
+-- HTML
+hi("@tag", { fg = colors.light_orange })
+hi("@tag.attribute", { fg = colors.brown })
+hi("@tag.delimiter", { fg = colors.orange })
+hi("@punctuation.special", { fg = colors.green })
 
 -- Hide NonText characters by default.
 -- There's function that will make them visible in .vimrc.
@@ -54,7 +111,7 @@ hi("NonText", { fg = colors.dark_gray })
  -- UI Elements
 hi("ColorColumn", { bg = "#130707" })
 hi("Conceal", { fg = colors.white, bg = colors.bg })
-hi("Cursor",  { fg = colors.bg, bg = "#F5773c" })
+hi("Cursor",  { fg = colors.bg, bg = colors.orange })
 hi("CursorColumn", { bg = "#2D2D2D" })
 hi("CursorLine", { bg = "#131313", bold = true })
 hi("Directory", { fg = "#CFCFCF" })
@@ -62,28 +119,29 @@ hi("FoldColumn", { fg = "#444444", bg = colors.bg })
 hi("Folded",  { bg = colors.bg, fg = "#434343", italic = true })
 hi("LineNr", { bg = colors.bg, fg = "#434343" })
 hi("MatchParen",  { fg= colors.red, bg = colors.bg })
-hi("Normal", { bg = "#1D1D1D" })
+hi("Normal", { bg = colors.bg })
 hi("Pmenu",  { fg = "#F6F3E8", bg = "#242829" })
 hi("PmenuSel", { fg = "#000000", bg = colors.light_green })
-hi("SignColumn", { bg ="#1D1D1D" })
+hi("CurSearch", { bg = colors.orange, fg = colors.bg })
+hi("Search", { bg = colors.light_green, fg = colors.bg })
+hi("SignColumn", { bg = colors.bg })
 hi("TabLineSel", { fg = colors.light_green })
 hi("Visual",  { fg = "NONE", bg = "#1F4457", })
 hi("WildMenu", { fg = colors.light_red, bg = colors.bg })
 hi("WinSeparator", { fg = "#181B22", bg = colors.bg })
 
--- https://github.com/nvim-treesitter/nvim-treesitter/blob/master/lua/nvim-treesitter/highlight.lua
-hi("TSConstant", { fg = colors.light_red })
-hi("TSKeywordReturn", { fg = colors.salmon, italic = true })
-
 -- LSP
 hi("LspInlayHint", { fg = colors.light_gray, bg = "bg", italic = true })
 
 -- Warnings and Errors
-hi("DiagnosticError", { fg = colors.red, bg = "bg" })
-hi("Error", { fg = colors.red, bg = "bg" })
-hi("DiagnosticWarn", { fg = "#E7F6DA", bg = "bg" })
-hi("Warning", { fg = "#E7F6DA", bg = "#3A0505" })
-hi("WarningMsg", { fg = colors.light_yellow, bg = colors.bg })
+hi("DiagnosticError", { fg = colors.light_red })
+hi("DiagnosticUnderlineError", { fg = colors.light_red, undercurl = true })
+hi("DiagnosticWarn", { fg = colors.light_orange })
+hi("DiagnosticUnderlineWarn", { undercurl = true })
+hi("DiagnosticInfo", { fg = colors.light_green })
+hi("Error", { fg = colors.red })
+hi("Warning", { fg = colors.orange })
+hi("WarningMsg", { fg = colors.light_yellow })
 
 -- Status Line
 hi("StatusLine", { fg = colors.light_gray, bg = colors.bg })
@@ -109,6 +167,7 @@ hi("FloatBorder", { fg = colors.middle_gray, bg = colors.middle_gray })
 hi("FloatTitle", { fg = colors.blue, bg = colors.middle_gray })
 hi("NormalFloat", { bg = colors.middle_gray })
 
+-- Noice
 hi("NoiceBorder", { fg = "#2A2A2A", bg = "#2A2A2A" })
 hi("NoiceCmdlinePopupBorderCmdline", { fg = "#2A2A2A", bg = "#2A2A2A", blend = 1 })
 hi("NoiceCmdlinePopupBorderFilter", { fg = "#2A2A2A", bg = "#2A2A2A" })
@@ -143,8 +202,8 @@ hi("TelescopeResultsTitle", { bg = colors.middle_gray })
 -- Signify
 hi("GitGutterAdd", { fg = "#7D8D57" })
 hi("GitGutterAddLine", { bg ="#191919" })
-hi("GitGutterChange", { fg = "#f5773C", bg = "NONE" })
-hi("GitGutterChangeDelete",  { fg = "#f5773C", bg = "NONE" })
+hi("GitGutterChange", { fg = colors.orange, bg = "NONE" })
+hi("GitGutterChangeDelete",  { fg = colors.orange, bg = "NONE" })
 hi("GitGutterChangeDeleteLine", { bg = "#191919" })
 hi("GitGutterChangeLine", { bg = "#191919" })
 hi("GitGutterDelete", { fg = colors.red })
