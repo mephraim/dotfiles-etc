7eaecf8efc046a2bf2893d7fcd92e81b4b2ed80a
Matthew Ephraim <me@mattephraim.com>
2024-12-04 21:29:17 -0600

    Try the codecompanion plugin

--- vim/lua/plugins/ai.lua
+++ vim/lua/plugins/ai.lua
@@ -3,16 +3,35 @@ return function(use)
     "olimorris/codecompanion.nvim",
     config = function()
       require("codecompanion").setup({
+        display = {
+          chat = {
+            window = {
+              width = .35
+            }
+          }
+        },
         strategies = {
           chat = {
             adapter = "copilot",
-            render_headers = false
           },
 
           inline = {
             adapter = "copilot",
           },
         },
+      })
+
+      local companionGroup = vim.api.nvim_create_augroup(
+        "Conceal2Spaces", { clear = true })
+
+      vim.api.nvim_create_autocmd(("BufEnter"), {
+        pattern = "*",
+        callback = function()
+          if vim.bo.filetype == "codecompanion" then
+            vim.opt_local.number = false
+          end
+        end,
+        group = companionGroup
       })
     end,
 
